f0de10e979ddc13c476098d7d9f727273b7e54e3
RjaasielMT <74688123+RjaasielMT@users.noreply.github.com>
2023-07-18 17:26:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+  
+    menuToggle.addEventListener('click', function() {
+      menuToggle.classList.toggle('open');
+      menu.classList.toggle('open');
+    });
+  });
+  
